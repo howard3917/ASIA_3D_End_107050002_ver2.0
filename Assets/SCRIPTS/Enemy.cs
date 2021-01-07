c09b4da8f9a015a965d2b512cafa689c7841a57a
howard <howard3917@gmail.com>
2021-01-07 23:00:33 +0800

    1/7 進度14

---
+++ Assets/SCRIPTS/Enemy.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 50)]
+    public float speed = 3;
+    [Header("停止距離"), Range(0, 50)]
+    public float stopDistance = 0.1f;
+    [Header("攻擊冷卻"), Range(0, 50)]
+    public float cd = 2f;
+    [Header("攻擊長度"), Range(0f, 5f)]
+    public float atkLength;
+    [Header("攻擊中心點")]
+    public Transform atkpoint;
+
+
+    private Transform player;
+    private NavMeshAgent nav;
+    private Animator ani;
+
+    private float timer;
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(atkpoint.position, atkpoint.forward * atkLength);
+    }
+
+    private void Awake()
+    {
+        ///取得身上的元件(代理器)
+        nav = GetComponent<NavMeshAgent>();
+        ani = GetComponent<Animator>();
+        ///尋找其他遊戲物件("物件名稱").變形元件
+        player = GameObject.Find("AAAAA").transform;
+
+        nav.speed = speed;
+        nav.stoppingDistance = stopDistance;
+    }
+
+    private void Update()
+    {
+        track();
+        Attack();
+    }
+
+
+    private RaycastHit hit;
+
+    private void Attack()
+    {
+        if (nav.remainingDistance < stopDistance)
+        {
+            timer += Time.deltaTime;
+
+            Vector3 pos = player.position;
+
+            pos.y = transform.position.y;
+
+            transform.LookAt(pos);
+
+            if (timer >= cd)
+            {
+                ani.SetTrigger("攻擊觸發");
+
+                timer = 0;
+
+                if (Physics.Raycast(atkpoint.position, atkpoint.forward, out hit, atkLength, 1 << 8))
+                {
+                    hit.collider.GetComponent< Player>().Damage();
+                }
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// 追蹤
+    /// </summary>
+    private void track()
+    {
+        ///代理器.設定目的地(玩家座標)
+        nav.SetDestination(player.position);
+
+        ///print("剩餘距離:" + nav.remainingDistance);
+        ani.SetBool("跑步開關", nav.remainingDistance > stopDistance);
+
+    }
+}
---
+++ Assets/SCRIPTS/NPC.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NPCData data;
+    [Header("對話框")]
+    public GameObject dialog;
+    [Header("對話內容")]
+    public Text textContent;
+    [Header("對話者名稱")]
+    public Text textName;
+    public bool playerInArea;
+    [Header("對話間隔")]
+    public float interval = 0.2f;
+
+    ///private void start()
+    ///{
+    ///    StartCoroutine(Test());
+
+    ///}
+
+    ///private IEnumerator Test()
+    ///{
+    ///    print("hello");
+    ///    yield return new WaitForSeconds(1.5f);
+    ///    print("hellooooooooooooooo");
+
+    ///}
+    public enum NPCState
+    {
+        First, Missioning, Finish
+    }
+    public NPCState state = NPCState.First;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "AAAAA")
+        {
+            playerInArea = true;
+            StartCoroutine(Dialog());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "AAAAA")
+        {
+            playerInArea = false;
+            StopDialog();
+        }
+    }
+    /// <summary>
+    /// 停止對話
+    /// </summary>
+    private void StopDialog()
+    {
+        {
+            dialog.SetActive(false);   ///關閉對話框
+            StopAllCoroutines();       ///停止所有協成
+        }
+    }
+    /// <summary>
+    ///開始對話
+    /// </summary>
+
+    private IEnumerator Dialog()
+    {
+        /// print(data.dialougA);
+        /// print(data.dialougA[0])
+
+        ///for  (int  i  =  0;  i  < 10;   i++)
+        //		{
+        // print("迴圈:"+i);
+        dialog.SetActive(true);
+        textContent.text = "";
+        textName.text = name;
+
+
+        string dialogString = data.dialogB;
+
+        switch (state)
+        {
+            case NPCState.First:
+                dialogString = data.dialogA;
+                break;
+            case NPCState.Missioning:
+                dialogString = data.dialogB;
+                break;
+            case NPCState.Finish:
+                dialogString = data.dialogC;
+                break;
+
+        }
+
+        for (int i = 0; i < data.dialogA.Length; i++)
+        {
+            textContent.text += data.dialogA[i] + "";
+            ///   print(data.dialougA[i]);
+
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+
+
+
+}
